68b5037a462fc2b6950f3e9e3f307a696771d69f
wilsonm8 <103072620@student.swin.edu.au>
2025-01-31 22:17:30 +1100

    passed in props to board component from game component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ function Square({value, onSquareClick}){
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  
+  function Board({xIsNext, squares, onPlay}) {
+
   const winner = calculateWinner(squares);
   let status;
 
@@ -35,9 +33,7 @@ function Board() {
     else{
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares); 
   }
   return (
     <>
@@ -63,19 +59,25 @@ function Board() {
 }
 
 export default function Game(){
-  const [xIsNext, setXisNext] = useState(true);
+  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  
   const currentSquares = history[history.length-1];
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
   return(
     <div className ="game">
       <div className = "game-board">
-        <Board/>
+       <Board xIsNext={xIsNext} squares = {currentSquares} onPlay = {handlePlay}/>
       </div>
       <div className = "game-info">
         <ol>{/*TODO*/}</ol>
       </div>
     </div>
   )
+  
 }
 
 function calculateWinner(squares) {
