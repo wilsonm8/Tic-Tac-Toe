ee220ad61c3e5ac196ddfc362c54793d57c8c01a
wilsonm8 <103072620@student.swin.edu.au>
2025-02-01 18:52:46 +1100

    created description for user to jump back to moves
    still need to implement jump back functionality

--- src/App.js
+++ src/App.js
@@ -67,13 +67,32 @@ export default function Game(){
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove){
+    //TODO
+  }
+
+  const moves = history.map((squares,move) =>{
+    let description;
+    if(move>0){
+      description = "Go to move #" + move;
+    }
+    else{
+      description = "Go to game start";
+    }
+    return(
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
   return(
     <div className ="game">
       <div className = "game-board">
        <Board xIsNext={xIsNext} squares = {currentSquares} onPlay = {handlePlay}/>
       </div>
       <div className = "game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
